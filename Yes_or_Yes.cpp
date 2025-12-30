d09136bf0dbd221775bce765e858d71b68dfd42f
dibyajit1234 <dibyajitnandi7205@gmail.com>
2025-12-30 21:45:16 +0530

    updated

---
+++ Yes_or_Yes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    string s;
+    cin>>s;
+    int count=0;
+    for(int i=0;i<s.length();i++){
+      if(s[i]=='Y')count++;
+    }
+    if(count>=2)cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+  }
+  return 0;
+}
