0cb8369c08762dd24f78615e492ed450aafe4f2e
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-01-01 13:42:42 +0530

    updated

---
+++ Buttons.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if((abs(a-b)+c)%2!=0) cout<<"First"<<endl;
+    else cout<<"Second"<<endl;
+  }
+  return 0;
+}
