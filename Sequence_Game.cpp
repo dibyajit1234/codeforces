0475fd02f3f32036f85b4ebe33b3d67b207e1c03
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-01-01 10:43:32 +0530

    updated

---
+++ Sequence_Game.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin>> t;
+  while(t--){
+    int n;
+    cin>>n;
+    vector<int> a;
+    for(int i=0;i<n;i++){
+      int x;
+      cin>>x;
+      if(i && a.back()>x) a.push_back(1);
+      a.push_back(x);
+    } 
+    cout<<a.size()<<endl;
+    for(int x:a){
+      cout<<x<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
