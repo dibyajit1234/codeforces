ecc7ac9764f3a6c4afceffebbab0632c59ffbfa9
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-01-04 13:08:05 +0530

    updated

--- One_and_Two.cpp
+++ One_and_Two.cpp
@@ -5,20 +5,24 @@ int main(){
   int t;
   cin>>t;
   while(t--){
-    int n,mul=1,temp =1,res =-1;
+    int n,i=0,k=0,j=n-1,res =-1;
     cin>>n;
     vector<int> a(n);
-    for(int &x:a){
-      cin>>x;
-      mul*=x;
-    }
-    for(int i=0;i<n;i++){
-      temp *=a[i];
-      if(mul/temp==temp){
-        res = i+1;
-        break;
+    for(int &x:a)cin>>x;
+    while(k<=n-2){
+      int c1=0,c2=0;
+      for(int i=0;i<=k;i++){
+        if(a[i]==2) c1++;
       }
+      for(int i=k+1;i<n;i++){
+        if(a[i]==2) c2++;
+      }
+      if(c1==c2){
+        res=k+1;
+        break;
+      }else k++;
     }
     cout<<res<<endl;
   }
+  return 0;
 }
