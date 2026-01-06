b3d3f70e10c3f16e9c3f2c9aed07bbbe19fc499b
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-01-06 22:21:43 +0530

    updated

--- Comparison_String.cpp
+++ Comparison_String.cpp
@@ -4,14 +4,16 @@ int main(){
   int t;
   cin>>t;
   while(t--){
-    int n,count=0;
+    int n,count=1,ans =1;
     cin>>n;
     string s;
     cin>>s;
     for(int i=1;i<n;i++){
       if(s[i]==s[i-1])count++;
+      else count =1;
+      ans = max(ans,count);
     }
-    cout<<(n%2==0?count+1:count+2)<<endl;
+    cout<<ans+1<<endl;
   }
   return 0;
 }
