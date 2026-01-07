65778484c795b672e9c8fb2e1a211c1df85a323e
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-01-07 12:05:25 +0530

    updated

--- Odd_Queries.cpp
+++ Odd_Queries.cpp
@@ -7,17 +7,17 @@ int main(){
   while(t--){
     int n,q;
     cin>>n>>q;
-    vector<int> a(n),as(n);
-    as.push_back(0);
+    int a[n],as[n];
+    as[0]=0;
     for(int i=1;i<=n;i++){
       cin>>a[i];
       as[i]=as[i-1]+a[i];
     }
     while(q--){
-      int l,r,k,temp=0;
+      int l,r,k;
       cin>>l>>r>>k;
       int sum = as[n]-as[r]+as[l-1];
-      bool ans = (sum-(r-l+1)*k)%2==0;
+      bool ans = (sum+(r-l+1)*k)%2==0;
       if(ans)cout<<"No"<<endl;
       else cout<<"Yes"<<endl;
     }
