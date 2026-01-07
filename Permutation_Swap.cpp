62a95757fff04b2729dc5ddf6f25631370db6881
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-01-07 10:30:24 +0530

    updated

--- Permutation_Swap.cpp
+++ Permutation_Swap.cpp
@@ -1,19 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+int gcd(int a,int b){
+  if(b==0)return a;
+  return gcd(b,a%b);
+}
+
 int main(){
   int t;
   cin>>t;
   while(t--){
-    int n;
+    int n,ans=0;
     cin>>n;
-    vector<int> a(n);
-    for(int &x:a)cin>>x;
-    int m=1,temp = n;
-    while(temp/10>0) {
-      m++;
-      temp = temp/10;
+    for(int i=1;i<=n;i++){
+      int x;
+      cin>>x;
+      ans = gcd(ans,abs(x-i));
     }
-    cout<<(n%2==0?(int)(n/pow(2,m)):(int)((n/pow(2,m))+(m-1)))<<endl;
+    cout<<ans<<endl;
+
   }
   return 0;
 }
+//In order to move pi to its right position, what does the value of k have to satisfy?
