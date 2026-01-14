d0fc6e90dd7d89f4a9c6399151d5690f79fd7325
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-01-14 22:05:58 +0530

    updated

--- Yuu_Koito_and_Minimum_Absolute_Sum.cpp
+++ Yuu_Koito_and_Minimum_Absolute_Sum.cpp
@@ -8,21 +8,13 @@ int main(){
     cin>>n;
     vector<int>a(n);
     for(int &x:a)cin>>x;
-    if(n==2 ){
-      if(a[0]==-1)a[0]=abs(a[1]);
-      else if(a[1]==-1)a[1]=abs(a[0]);
+    if(a[0]==-1)a[0]=a[n-1];
+    if(a[n-1]==-1) a[n-1]=a[0];
+    for(int i=0;i<n;i++){
+      if(a[i]==-1)a[i]=0;
     }
-    else{
-      for(int i=0;i<n-1;i++){
-        if(a[i]==-1)a[i]=0; 
-      }
-      for(int i=0;i<n-2;i++) sum+=a[i+1]-a[i];
-      if(a[n-1]==-1)a[n-1]=(sum>0?0:a[n-2]+abs(sum));
-    }    
-    
-    sum+=a[n-1]-a[n-2];
-    cout<<abs(sum)<<endl;
-    for(int i:a)cout<<i<<' ';
+    cout<<abs(a[n-1]-a[0])<<endl;
+    for(int i=0;i<n;i++)cout<<a[i]<<' ';
     cout<<'\n';
   }
   return 0;
