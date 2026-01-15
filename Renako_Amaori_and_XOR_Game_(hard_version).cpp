53493a5e3af20fdc5687f19b813ba2c59a65fdcd
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-01-15 12:35:41 +0530

    updated

--- Renako_Amaori_and_XOR_Game_(hard_version).cpp
+++ Renako_Amaori_and_XOR_Game_(hard_version).cpp
@@ -1,29 +1,30 @@
 #include<bits/stdc++.h>
+typedef long long ll;
 using namespace std;
 int main(){
   int t;
   cin>>t;
   while(t--){
-    int n,ac=0,bc=0;
+    ll n,ac=0,bc=0;
     cin>>n;
-    vector<int> a(n),b(n);
-    for(int &x:a){
+    vector<ll> a(n),b(n);
+    for(ll &x:a){
       cin>>x;
       ac^=x;
     }
-    for(int &x:b){
+    for(ll &x:b){
       cin>>x;
       bc^=x;
     }
-    for(int i=0;i<n;i++){
+    for(ll i=0;i<n;i++){
       if(i%2==0){
-        int temp = (ac^a[i])^b[i];
+        ll temp = (ac^a[i])^b[i];
         if(temp>ac){
           ac=temp;
           bc=(bc^b[i])^a[i];
         }
       }else{
-        int temp = (bc^b[i])^a[i];
+        ll temp = (bc^b[i])^a[i];
         if(temp>bc){
           bc = temp;
           ac = (ac^a[i])^b[i];
