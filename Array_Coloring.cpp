bde018f08e5e179641c214c1cc2ca3c9b1fc961e
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-01-17 22:14:39 +0530

    updated

--- Array_Coloring.cpp
+++ Array_Coloring.cpp
@@ -1,18 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool check_even(int a){
+  return a%2==0;
+}
 int main(){
   int t;
   cin >> t;
   while(t--){
     int n;
+    bool flag=true;
     cin>>n;
     vector<int> a(n);
     int sum=0;
     for(int &x:a){
       cin>> x;
-      sum+=x;
     }
-    if(sum%2==0)cout<<"Yes"<<endl;
+    for(int i=1;i<n;i++){
+      if(check_even(a[i]) == check_even(a[i-1])){
+        flag = false;
+        break;
+      }
+    }
+    if(flag) cout<<"Yes"<<endl;
     else cout<<"No"<<endl;
   }
   return 0;
