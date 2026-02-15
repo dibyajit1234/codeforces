3cd8325df1b3e17fc4f4cc9121e0ab80e7bb3028
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-02-15 20:50:39 +0530

    updated

--- Sieve_of_Erato67henes.cpp
+++ Sieve_of_Erato67henes.cpp
@@ -1,21 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   int t;
   cin>>t;
   while(t--){
     int n;
     cin>>n;
-    bool ans = false;
+    bool ss=false,one = false;
+
     while(n--){
       int x;
       cin>>x;
-      if(x%67==0) {
-        ans = true;
-        break;
-      }
+      if(x==67)ss = true;
+      if(x==1) one = true;
     }
-    cout<<(ans?"Yes":"No")<<'\n';
+    
+    cout<<((ss && one)?"Yes":"No")<<'\n';
   }
   return 0;
 }
