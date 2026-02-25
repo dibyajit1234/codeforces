86d8589cd6aae118b36eca7b6a4f8316e6c295fb
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-02-25 21:36:09 +0530

    updated

--- Beautiful_Numbers.cpp
+++ Beautiful_Numbers.cpp
@@ -8,11 +8,31 @@ int main(){
   while(t--){
     string s;
     cin>>s;
-    vector<int> count(9,0);
-    for(char c:s)count[c-'0']++;
-    int m =0;
-    for(int x:count){m = max(m,x);}
-    cout<<(s.size()-m)<<'\n';
+    int sum =0,n=s.size();
+    vector<int> count;
+    for(char c:s){
+      sum+=c-'0';
+    }
+    if(sum<10){
+      cout<<0<<'\n';
+      continue;
+    }
+      int ans =0,need = sum-9;
+      for(int i=0;i<n;i++){
+        int d = s[i]-'0';
+        if(i==0){
+        count.push_back(d-1);
+        }else count.push_back(d);
+    }
+
+      sort(count.rbegin(),count.rend());
+    for(int x:count){
+      if(need<=0)break;
+      need-=x;
+      ans++;
+    }
+    cout<<ans<<'\n';
+    
   }
   return 0;
 }
