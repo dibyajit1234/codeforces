2992e2e5ae1a3388282ca4e8abdafab3e0286028
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-02-27 20:53:02 +0530

    updated

---
+++ Eating_Game.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin>>t;
+  while(t--){
+    int n,mx=0,s=0;
+    cin>>n;
+    vector<int> a(n);
+    for(int &x:a)cin>>x;
+    for(int i=0;i<n;i++){
+      int x = a[i];
+      s+=x;
+      mx = max(mx,x);
+    }
+    int count =0;
+    for(int x:a)if(x==mx)count++;
+    cout<<count<<'\n';
+    
+  }
+  return 0;
+}
