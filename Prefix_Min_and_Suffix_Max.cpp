56b4b386ef48af01bcb07d58b094a3f5b599e99c
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-03-31 12:22:58 +0530

    updated

--- Prefix_Min_and_Suffix_Max.cpp
+++ Prefix_Min_and_Suffix_Max.cpp
@@ -6,21 +6,26 @@ int main(){
   int t;
   cin>>t;
   while(t--){
-    int n,mx=0,mn=0;
+    int n;
     cin>>n;
-    vector<int> a(n),ans(n);
+    vector<int> a(n),pref(n),sufx(n);
     for(int &x:a)cin>>x;
+    int mx=a[n-1],mn=a[0];
     for(int i=0;i<n;i++){
-      mx=(a[i]>a[mx]?i:mx);
-      mn=(a[i]<a[mn]?i:mn);
+      mn=min(mn,a[i]);
+      pref[i]=mn;
     }
-    int flag1=(mn==0?-1:mn-1),flag2=(mx==n-1?-1:mx+1);
+    for(int i=n-1;i>=0;i--){
+      mx=max(mx,a[i]);
+      sufx[i]=mx;
+    }
     for(int i=0;i<n;i++){
-      if(i==mx||i==mn||i==flag1||i==flag2)ans[i]=1;
-      else ans[i]=0;
+      if(pref[i]<a[i] && sufx[i]>a[i])cout<<0;
+      else cout<<1;
     }
-    for(int i:ans)cout<<i;
     cout<<'\n';
+
+    
   }
   return 0;
 }
