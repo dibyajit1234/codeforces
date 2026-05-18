fe6afa5e4d54b801319cedf3c776bacf5b9fbc81
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-05-18 22:21:00 +0530

    updated

--- Arrange_the_Numbers_in_a_Circle.cpp
+++ Arrange_the_Numbers_in_a_Circle.cpp
@@ -7,20 +7,24 @@ int main(){
   int t;
   cin>>t;
   while(t--){
-    int n;
+    ll n,ans=0,tot=0,singles=0,extra=0;
     cin>>n;
     vector<ll> c(n) ;
-    for(ll &x:c)cin>>x;
-    ll ans =0;
-    for(int i=0;i<n;i++)ans = max(ans,c[i]);
-
-    for(int i=0;i<n;i++){
-      for(int j=i+1;j<n;j++){
-        ans = max(ans,2LL * min(c[i],c[j]));
+    for(ll &x:c){
+      cin>>x;
+      tot+=x;
+      if(x>=2){
+        ans+=x;
+        extra = (x-2)/2;
       }
+      else singles++;
     }
+    ans+= min(singles,extra);
+    	ll other = min(tot, c[n-1] + c[n-1] / 2);
+	ans = max(ans, other);
     if(ans<3)ans=0;
     cout<<ans<<'\n';
+
   }
   return 0;
 }
