258492192aaffd1225057beed535a9be70b94f50
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-06-12 21:39:40 +0530

    solution added

--- Omsk_Programmers.cpp
+++ Omsk_Programmers.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 int main(){
   ios::sync_with_stdio(false);
@@ -26,4 +26,40 @@ int main(){
     }
     cout<<ans<<'\n';
   }
+}*/ 
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+void xxx(ll cur, ll cost, ll x,
+         unordered_map<ll,ll>& dist) {
+    auto it = dist.find(cur);
+    if (it != dist.end() && it->second <= cost) return;
+    dist[cur] = cost;
+    if (cur == 0) return;
+    ll q = cur / x;
+    ll r = cur % x;
+    xxx(q, cost + 1, x, dist);
+    if (r) {
+        xxx(q + 1, cost + (x - r) + 1, x, dist);
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        ll a, b, x;
+        cin >> a >> b >> x;
+        unordered_map<ll,ll> distA, distB;
+        xxx(a, 0, x, distA);
+        xxx(b, 0, x, distB);
+        ll ans = llabs(a - b);
+        for (auto &[u, ca] : distA) {
+            for (auto &[v, cb] : distB) {
+                ans = min(ans, ca + cb + llabs(u - v));
+            }
+        }
+        cout << ans << '\n';
+    }
 }
