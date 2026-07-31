5de2fec57ad245ca99b35a39f01cded38cd95532
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-08-01 02:15:27 +0530

    updated

---
+++ Hexadecimals_theorem.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hexadecimals_theorem{
+  public static void main(String a[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.print(0);
+    System.out.print(0);
+    System.out.println(n);
+  }
+}
