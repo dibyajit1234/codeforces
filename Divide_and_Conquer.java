2a3e65ef7b1dd2184c3a61e6a1c144c56a301480
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-08-01 14:37:51 +0530

    updated

---
+++ Divide_and_Conquer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Divide_and_Conquer{
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while(t-->0){
+      int x = sc.nextInt(),y = sc.nextInt();
+      System.out.println(x%y==0?"Yes":"No");
+    }
+  }
+}
